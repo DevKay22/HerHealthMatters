4263dd75d9656535942ef5f5983c8464fd370521
Kayode <drkay4sure@gmail.com>
2025-04-07 23:23:00 +0100

    done with herobanner

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../src/Components/Header';
+import Hero from '../src/Components/Hero'
 
 const App = () => {
   return (
     <div>
       <Header />
+      <Hero />
     </div>
   )
 }
---
+++ src/Components/Hero.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import '../Styles/Hero.css'
+import herhealth from '../assets/Images/Sonography-bro 1.png';
+import women from '../assets/Images/women.png'
+const Hero = () => {
+  return (
+    <div className='herobanner'>
+        <div className='carousel'>
+            <div className='cont'>
+                <h1>
+                    Online Health Care for Women
+                </h1>
+
+                <p>
+                    From pregnancy to birth, HerHealthMatters  is here to care for you.
+
+                </p>
+
+                <button>Let's begin the journey!</button>
+            </div>
+            <div className='imagery'>
+                <img src={herhealth} alt="" />
+            </div>
+        </div>
+
+
+        <div className='womencont'>
+            <div>
+                <p>
+                Join our community of women
+                </p>
+            </div>
+            
+            <div>
+                <img src={women} alt="" />
+            </div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Hero
