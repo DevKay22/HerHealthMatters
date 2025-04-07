21674aea8435c1b1541a49a0ef3f1d219e4c4501
Kayode <drkay4sure@gmail.com>
2025-04-07 22:08:33 +0100

    almost done with the head tag

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,21 +1,47 @@
 import React from 'react';
 import '../Styles/Header.css'
-import logo from '../assets/Images/HerHealthMatters.png'
+import logo from '../assets/Images/HerHealthMatters.png';
+import { GiHamburgerMenu } from "react-icons/gi";
 const Header = () => {
   return (
-    <div className='head'>
+    <div className='content'>
+        <div className='head'>
         <div className='logo'>
             <img src={logo} alt="" />
         </div>
         <div className='items'>
-            <p>Home</p>
-            <p>About</p>
-            <p>Blog</p>
-            <p>Our services</p>
+            <p className='home'>Home</p>
+            <p className='grey navhover'>About</p>
+            <p className='grey navhover'>Blog</p>
+            <p className='grey navhover'>Our services</p>
                 
             
         </div>
-        <div className='login'>C</div>
+        <div className='login'>
+            <button type='button'>Sign in</button>
+        </div>
+
+        {/* mobile */}
+
+        
+        <div className='hamburger'>
+           <GiHamburgerMenu  className='ham'/>
+        </div>
+        
+
+        
+        <div className='mobileItems'>
+            <p className='home'>Home</p>
+            <p className='grey'>About</p>
+            <p className='grey'>Blog</p>
+            <p className='grey'>Our services</p>
+        </div>
+        
+        
+        
+       
+    </div>
+
     </div>
   )
 }
