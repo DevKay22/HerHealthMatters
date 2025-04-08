da061cdd9b75de5970d9787b3b432e4e95dd2544
Kayode <drkay4sure@gmail.com>
2025-04-08 08:53:50 +0100

    services dv

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../src/Components/Header';
-import Hero from '../src/Components/Hero'
+import Hero from '../src/Components/Hero';
+import Services from '../src/Components/Services';
 
 const App = () => {
   return (
     <div>
       <Header />
       <Hero />
+      <Services />
     </div>
   )
 }
---
+++ src/Components/Services.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import '../Styles/Services.css';
+import icon1 from '../assets/Images/icon1.png';
+import icon2 from '../assets/Images/icon2.png';
+import icon3 from '../assets/Images/icon3.png';
+
+const Services = () => {
+  return (
+    <div className='services'>
+        <div className='svc'>
+            <h1>
+                Our Services
+            </h1>
+
+            <div className='svcContent'>
+                <div>
+                  <img src={icon1} alt="" className='icons' />
+
+                  <h3>
+                    Maternity Tips and Guides
+                  </h3>
+
+                  <p>
+                    Whether you are planning to become pregnant, already pregnant or about to 
+                    become a parent, there are various tips and guides to help you navigate 
+                    this journey.
+                  </p>
+                </div>
+
+
+                <div>
+                <img src={icon2} alt="" className='icons' />
+
+                <h3>
+                   Free Consultation
+                </h3>
+
+                <p>
+                    Meet our professionals for free. Discuss your needs, ask questions and get answers, 
+                    discuss options, establish a relationship and potentially schedule follow-up appointments.
+                </p>
+                </div>
+
+
+
+                <div>
+                <img src={icon3} alt="" className='icons' />
+
+                  <h3>
+                    Pharmaceuticals
+                  </h3>
+
+              <p>
+                 We have a broad category of drugs and medicataions designed for various medical purposes.
+              </p>
+            </div>
+
+
+            </div>
+
+            <div className='btn'>
+            <button>Read More</button>
+            </div>
+        </div>
+
+        
+    </div>
+  )
+}
+
+export default Services
